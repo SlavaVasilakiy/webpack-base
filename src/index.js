7e7cae99cf4e8f8fad5b83a501386354399d11bd
SlavaVasilakiy <slk55slk@gmail.com>
2024-02-23 08:43:43 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.html';
+import './index.scss';
+import { multiply, summary } from './modules/calc';
+
+console.log(multiply(2, 3));
+console.log(summary(6, 5));
---
+++ src/modules/calc.js
@@ -0,0 +1,7 @@
+export function multiply(a, b) {
+	return a * b;
+}
+
+export async function summary(a, b) {
+	return a + b;
+}
